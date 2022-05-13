ff326add3cf091562e12daecfcef5fe178f4fcb5
Somesh Debnath <91829500+Somesh-Debnath@users.noreply.github.com>
2022-05-13 21:07:40 +0530

    final

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,32 @@
-import Meta from '../components/Meta'
-import {server} from '../config'
+import { server } from '../config'
 import ArticleList from '../components/ArticleList'
-export default function Home({articles}) {
 
+export default function Home({ articles }) {
   return (
-    <div >
-      <Meta/>
-      <h1>Welcome to my Website
-      </h1>
+    <div>
       <ArticleList articles={articles} />
-     </div>
+    </div>
   )
 }
-export async function getStaticProps() {
-  const res=await fetch(`${server}/api/articles`)
-  const articles=await res.json()
+
+export const getStaticProps = async () => {
+  const res = await fetch(`${server}/api/articles`)
+  const articles = await res.json()
+
   return {
-    props:
-    {
-       articles
+    props: {
+      articles,
     },
   }
 }
-// export async function getStaticProps() {
-//   const res=await fetch('https://jsonplaceholder.typicode.com/posts?_limit=6')
-//   const articles=await res.json()
+
+// export const getStaticProps = async () => {
+//   const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=6`)
+//   const articles = await res.json()
+
 //   return {
-//     props:
-//     {
-//        articles
+//     props: {
+//       articles,
 //     },
 //   }
 // }
